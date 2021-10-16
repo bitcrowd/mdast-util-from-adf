e9d60f0bcd43443bf107b535db65087077bb6d48
Paul Meinhardt <paul@bitcrowd.net>
2021-10-17 01:26:31 +0200

    Switch approaches: Use ADF and convert to `mdast`
    Rather than trying to parse the textual Jira/Confluence markup notation,
    use the newer "Atlassian Document Format" (ADF) as input for converting
    to markdown. The format structure is documented here:
    https://developer.atlassian.com/cloud/jira/platform/apis/document/structure/
    
    It is a tree structure which should lend itself to be transformed into a
    valid "mdast" (https://github.com/syntax-tree/mdast) tree.

--- index.test.ts
+++ index.test.ts
@@ -1,75 +1,9 @@
-import fs from "fs/promises";
-import path from "path";
-import stringify from "remark-stringify";
-import { unified } from "unified";
+import { u } from "unist-builder";
 
-import { parse, parser, remark, transform } from ".";
+import convert from ".";
 
-function read(filepath: string): Promise<string> {
-  return fs.readFile(filepath, { encoding: "utf8" });
-}
-
-let example: string;
-
-beforeAll(async () => {
-  example = await read(path.join(__dirname, "example.txt"));
-});
-
-describe("parser", () => {
-  it("parses jira notation into a syntax tree", () => {
-    expect(parser(example)).toMatchInlineSnapshot(`
-      Object {
-        "children": Array [
-          Object {
-            "children": Array [
-              Object {
-                "type": "text",
-                "value": "Hello, ",
-              },
-              Object {
-                "children": Array [
-                  Object {
-                    "type": "text",
-                    "value": "World",
-                  },
-                ],
-                "type": "emphasis",
-              },
-              Object {
-                "type": "text",
-                "value": "!",
-              },
-            ],
-            "depth": 2,
-            "type": "heading",
-          },
-        ],
-        "type": "root",
-      }
-    `);
-  });
-});
-
-describe("transform", () => {
-  it("transforms a jira syntax tree into a markdown syntax tree", () => {
-    expect(transform({ type: "root", children: [] })).toEqual({
-      type: "root",
-      children: [],
-    });
-  });
-});
-
-describe("end to end", () => {
-  async function process(input: string): Promise<string> {
-    const pipeline = unified().use(parse).use(remark).use(stringify).freeze();
-    const file = await pipeline.process(input);
-    return String(file);
-  }
-
-  it("works", async () => {
-    expect(await process(example)).toMatchInlineSnapshot(`
-      "## Hello, *World*!
-      "
-    `);
-  });
+it("converts empty documents", () => {
+  expect(convert({ type: "doc", version: 1, content: [] })).toEqual(
+    u("root", [])
+  );
 });
--- index.ts
+++ index.ts
@@ -1,45 +1,11 @@
 import type { Root as MdastRoot } from "mdast";
-import type { Plugin } from "unified";
 
-type Root = MdastRoot; // TODO: extends with Jira node types
+type Root = any; // TODO: Define ADF types
 
-// Parser
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-export function parser(doc: string): Root {
-  const mock = {
-    type: "root",
-    children: [
-      {
-        type: "heading",
-        depth: 2,
-        children: [
-          { type: "text", value: "Hello, " },
-          {
-            type: "emphasis",
-            children: [{ type: "text", value: "World" }],
-          },
-          { type: "text", value: "!" },
-        ],
-      },
-    ],
-  } as Root;
-
-  return mock;
+export default function convert(doc: Root): MdastRoot {
+  // TODO: Convert node to `MdastRoot`.
+  //
+  // Check `unist` utilities for tree traversal and transformation:
+  // https://github.com/syntax-tree/unist
+  return { type: "root", children: [] };
 }
-
-// Transform from Jira to Markdown AST
-
-export function transform(node: Root): MdastRoot {
-  return node as MdastRoot; // TODO: properly convert node to MdastRoot
-}
-
-// Plugins
-
-export const parse: Plugin<void[], string, Root> = function parse() {
-  Object.assign(this, { Parser: parser });
-};
-
-export const remark: Plugin<void[], Root, MdastRoot> = function remark() {
-  return transform;
-};
